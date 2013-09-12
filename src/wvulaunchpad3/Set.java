c9388c310c9d41debf16460ba392bd7d581ed3ce
vonclites <alamos52@gmail.com>
2013-09-12 14:28:47 -0400

    toXML() methods implemented for Set, Cell and Part.  Sets can construct and return an unindented xml String.  Could benefit from formatting.

--- src/wvulaunchpad3/Set.java
+++ src/wvulaunchpad3/Set.java
@@ -10,15 +10,32 @@ import java.util.regex.Pattern;
 
 /**
  *
- * @author calvr
+ * @author dom
  */
 public class Set {
     private ArrayList<Cell> cells;
-    private String name;
     
     public Set(File[] cellDirectories){
+        cells = new ArrayList<Cell>();
         for (File cellDirectory : cellDirectories){
             cells.add(new Cell(cellDirectory));
         }
+    }
+    
+    public String toXML(){
+        String xml = "<SetView value=\"on\">\n";
+        xml += "<Structure>\n";
+        xml += "<NumCells value=\""+cells.size()+"\"/>\n";
+        
+        for (int i = 0; i < cells.size(); i++){
+            xml += "<Cell"+i+">\n";
+            xml += cells.get(i).toXML();
+            xml += "</Cell"+i+">\n";
+        }
+        
+        xml += "</Structure>\n";
+        xml += "</SetView>\n";
+        System.out.println(xml);
+        return xml;
     }
 }
